8b5f214f025fee79c12af665760e27d2098fa383
Knot <akthakorn@gmail.com>
2022-09-18 14:43:13 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const clientId = "1020937367525265408";
+const DiscordRPC = require("discord-rpc");
+const RPC = new DiscordRPC.Client({ transport: "ipc" });
+
+DiscordRPC.register(clientId);
+
+async function setActivity() {
+  if (!RPC) return;
+  RPC.setActivity({
+    details: `Knot Presence`,
+    state: `Playing with RPC`,
+    startTimestamp: Date.now(),
+    largeImageKey: `logo`,
+    largeImageText: `largeImageText`,
+    smallImageKey: `logo`,
+    smallImageText: `smallImageText.`,
+    instance: false,
+    buttons: [
+      {
+        label: `Text Button`,
+        url: `https://www.google.com`,
+      },
+      {
+        label: `Text Button`,
+        url: `https://www.google.com`,
+      },
+    ],
+  });
+}
+
+RPC.on("ready", async () => {
+  setActivity();
+
+  setInterval(() => {
+    setActivity();
+  }, 15 * 1000);
+});
+
+RPC.login({ clientId }).catch((err) => console.error(err));
